79d7263bb19ea71961c0a2cbae5572b64d27f271
简简爱 <59829816+jianjianai@users.noreply.github.com>
2020-08-10 02:47:43 +0800

    复制功能写完

--- src/cn/jji8/Creatorplayer/Creator/kzq/fuzhi.java
+++ src/cn/jji8/Creatorplayer/Creator/kzq/fuzhi.java
@@ -165,7 +165,7 @@ public class fuzhi implements kzq{
             main.getmian().getLogger().warning("未找到取消按钮按钮简介配置，已重新生成");
             kk = new ArrayList<String>();
             kk.add("取消正在执行的填充");
-            pz.set("按钮简介",kk);
+            pz.set("取消按钮按钮简介",kk);
             baocun =true;
         }
         wpsj.setLore(k);
